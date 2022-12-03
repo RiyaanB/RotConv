87edee3839d4fe341f8972fb065932783c15ce53
Riyaan Bakhda <riyaanbakhda2@gmail.com>
2022-12-03 02:01:20 -0500

    Made test model suitable for Cifar-10

--- complex_rot_net.py
+++ complex_rot_net.py
@@ -8,11 +8,13 @@ class ComplexRotNet(nn.Module):
 		super().__init__()
 
 		self.conv0  = ComplexRotConv2d(3, 16, 5, mode='real')
-		self.conv1  = ComplexRotConv2d(16, 32, 5)
+		self.conv1  = ComplexRotConv2d(16, 32, 3, stride=2)
 		self.conv2  = ComplexRotConv2d(32, 64, 5)
 		self.conv3  = ComplexRotConv2d(64, 128, 5)
-		self.conv4  = ComplexRotConv2d(128, 256, 5)
-		self.fc_out = nn.Linear(256, c_dim).to(torch.cfloat)
+		self.conv4  = ComplexRotConv2d(128, 256, 5, stride=2)
+		self.linear1 = nn.Linear(256, 256).to(torch.cfloat)
+		self.activation = nn.ReLU()
+		self.linear2 = nn.Linear(256, c_dim)
 
 	def forward(self, x):
 		batch_size = x.shape[0]
@@ -22,8 +24,10 @@ class ComplexRotNet(nn.Module):
 		net = self.conv2.forward(net)
 		net = self.conv3.forward(net)
 		net = self.conv4.forward(net)
-		net = (net[:,0] + net[:,1]*1j).view(batch_size, self.fc_out.in_features, -1).mean(2)
-		out = nn.ReLU(torch.real(self.fc_out(net)))
+		net = (net[:,0] + net[:,1]*1j).view(batch_size, self.linear1.in_features, -1).mean(2)
+		out = self.activation(torch.real(self.linear1(net)))
+		out = self.linear2(out)
+		print(out.shape)
 		return out
 
 model = ComplexRotNet(128)
