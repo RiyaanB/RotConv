2039c471ea2928a72212403a8ac4262203ca4d25
Riyaan Bakhda <riyaanbakhda2@gmail.com>
2022-12-08 23:52:12 -0500

    Modified all models for better performance

--- models.py
+++ models.py
@@ -11,18 +11,16 @@ class RotInvariantNet(nn.Module):
 		super().__init__()
 
 		self.layers = nn.Sequential( 
-			RotInvariantConv2d(in_dim, 32, 7, padding=3, n_angles=n_angles),
+			RotInvariantConv2d(in_dim, 16, 5, stride=2, padding=1, n_angles=n_angles),
 			nn.ReLU(),
-			RotInvariantConv2d(32, 64, 5, stride=2, padding=2, n_angles=n_angles),
+			RotInvariantConv2d(16, 64, 5, stride=2, padding=0, n_angles=n_angles),
 			nn.ReLU(),
-			RotInvariantConv2d(64, 64, 5, stride=2, padding=2, n_angles=n_angles),
-			nn.ReLU(),
-			RotInvariantConv2d(64, 128, 5, stride=2, padding=2, n_angles=n_angles),
+			RotInvariantConv2d(64, 128, 5, stride=2, padding=0, n_angles=n_angles),
 			nn.ReLU(),
 			nn.Flatten(),
-			nn.Linear(128*4*4, 512), 
+			nn.Linear(128, 64), 
 			nn.ReLU(),
-			nn.Linear(512, out_dim),
+			nn.Linear(64, out_dim),
 		)
 
 	def forward(self, x):
@@ -37,16 +35,15 @@ class RotEqNet(nn.Module):
 		super().__init__()
 
 		self.layers = nn.Sequential(
-			RotEquivariantConv2d(3, 3, 9, padding=4, n_angles=n_angles, mode='real'),
-			RotEquivariantConv2d(3, 4, 7, stride=2, padding=3, n_angles=n_angles),
-			RotEquivariantConv2d(4, 4, 7, stride=2, padding=3, n_angles=n_angles),
-			RotEquivariantConv2d(4, 4, 7, stride=2, padding=3, n_angles=n_angles),
+			RotEquivariantConv2d(in_dim , 16, 5, stride=2, padding=1, n_angles=n_angles, mode='real'),
+			RotEquivariantConv2d(16, 64, 5, stride=2, padding=0, n_angles=n_angles),
+			RotEquivariantConv2d(64, 128, 5, stride=2, padding=0, n_angles=n_angles),
 			ToComplex(),
 			nn.Flatten(),
-			nn.Linear(4*4*4, 32).to(torch.cfloat),
+			nn.Linear(128, 64).to(torch.cfloat),
 			RealRelu(),
 			ToReal(),
-			nn.Linear(32, 10),
+			nn.Linear(64, out_dim),
 		)
 
 	def forward(self, x):
@@ -61,18 +58,17 @@ class ComplexRotEqNet(nn.Module):
 		super().__init__()
 
 		self.layers = nn.Sequential(
-			RotEquivariantConv2d(in_dim, 8, 7, padding=3, n_angles=n_angles, mode='real'),
-			RotEquivariantConv2d(8, 16, 5, stride=2, padding=2, n_angles=n_angles),
-			RotEquivariantConv2d(16, 32, 5, stride=2, padding=2, n_angles=n_angles),
-			RotEquivariantConv2d(32, 64, 5, stride=2, padding=2, n_angles=n_angles),
+			RotEquivariantConv2d(in_dim, 16, 5, stride=2, padding=1, n_angles=n_angles, mode='real'),
+			RotEquivariantConv2d(16, 64, 5, stride=2, padding=0, n_angles=n_angles),
+			RotEquivariantConv2d(64, 128, 5, stride=2, padding=0, n_angles=n_angles),
 			ToComplex(),
 			nn.Flatten(),
-			nn.Linear(64*4*4, 1024).to(torch.cfloat),
+			nn.Linear(128, 32).to(torch.cfloat),
 			RealRelu(),
-			nn.Linear(1024, 512),
+			nn.Linear(32, 32).to(torch.cfloat),
 			ToReal(),
 			nn.ReLU(),
-			nn.Linear(1024, out_dim),
+			nn.Linear(32, out_dim),
 		)
 
 	def forward(self, x):
@@ -86,18 +82,16 @@ class ConvNet(nn.Module):
 	def __init__(self, in_dim=3, out_dim=128):
 		super().__init__()
 		self.layers = nn.Sequential( 
-			nn.Conv2d(3, 32, 7, padding=3),
+			nn.Conv2d(in_dim, 16, 5, stride=2, padding=1),
 			nn.ReLU(),
-			nn.Conv2d(32, 64, 5, stride=2, padding=2),
+			nn.Conv2d(16, 64, 5, stride=2, padding=0),
 			nn.ReLU(),
-			nn.Conv2d(64, 64, 5, stride=2, padding=2),
-			nn.ReLU(),
-			nn.Conv2d(64, 128, 5, stride=2, padding=2),
+			nn.Conv2d(64, 128, 5, stride=2, padding=0),
 			nn.ReLU(),
 			nn.Flatten(),
-			nn.Linear(128*4*4, 512), 
+			nn.Linear(128, 64), 
 			nn.ReLU(),
-			nn.Linear(512, 10),
+			nn.Linear(64, out_dim),
 		)
 		print(sum(p.numel() for p in self.parameters()))
 
